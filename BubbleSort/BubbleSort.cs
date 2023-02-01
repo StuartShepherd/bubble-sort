ae256d1a13f1f3b1c6ee48128afb7c0813b9a725
Stuart Shepherd <stuart@createstudios.com.au>
2023-02-02 08:12:57 +1100

    Add project files.

---
+++ BubbleSort/BubbleSort.cs
@@ -0,0 +1,24 @@
+﻿namespace BubbleSort;
+
+public class BubbleSort
+{
+    public int[] Sort(int[] array)
+    {
+        var length = array.Length;
+
+        for (int i = 0; i < length - 1; i++)
+        {
+            for (int j = 0; j < length - i - 1; j++)
+            {
+                if (array[j] > array[j + 1])
+                {
+                    var temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+            }
+        }
+
+        return array;
+    }
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# Bubble Sort
+
+Bubble sort is a simple comparison-based sorting algorithm that repeatedly steps through the list, compares adjacent elements and swaps them if they are in the wrong order. 
+The algorithm gets its name from the way smaller elements "bubble" to the top of the list. 
+The process is repeated until the list is sorted.
